83de81028865449bd8b603b56ec12cb7a77e57ca
alunno <damicosalvato@gmail.com>
2018-11-22 10:59:25 +0100

    tolti gli spaghetti

--- src/embeddedmediaplayer/EmbeddedMediaPlayer.java
+++ src/embeddedmediaplayer/EmbeddedMediaPlayer.java
@@ -1,8 +1,10 @@
 package embeddedmediaplayer;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -17,49 +19,35 @@ import java.util.Set;
 @SuppressWarnings("SpellCheckingInspection")
 public class EmbeddedMediaPlayer extends Application {
 
-    private static final String INTRO =  "PrimoVideo.mp4", SUSPANCE = "SecondoVideo.mp4";
-    private static final int CANDIDATES = 150, WINNERS = 6;
-
-    public class MyBoolean {
-        private boolean bool = false;
-        void set(boolean bool){
-            this.bool = bool;
-        }
-        boolean get(){
-            return bool;
-        }
-    }
+    private static final String INTRO =  "INTRO.mp4", SUSPANCE = "SUSPANCE.mp4", PATH = "";
+    private static final int CANDIDATES = 8, WINNERS = 2;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("I.T.E.T. Leonardo Da Vinci");
         primaryStage.setFullScreen(true);
-
+        Group root = new Group();
+        Scene scene = new Scene(root, 1920, 1080);
         MediaView mediaView = new MediaView();
-        openNewVideo(mediaView, INTRO);
-        mediaView.getMediaPlayer().setOnEndOfMedia(() -> mediaView.getMediaPlayer().seek(Duration.ZERO));
-
-        Group pippo = new Group();
-        pippo.getChildren().add(mediaView);
-        Scene scene = new Scene(pippo, 1920, 1080);
-        scene.setRoot(pippo);
-        scene.setOnKeyPressed(event -> sayWinners(scene, mediaView));
+        openNewVideo(scene, mediaView, INTRO,
+                event -> sayWinners(scene, mediaView),
+                () -> mediaView.getMediaPlayer().seek(Duration.ZERO));
+        root.getChildren().add(mediaView);
+        scene.setRoot(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
-
+    private boolean[] listener = new boolean[2];
     @SuppressWarnings("StatementWithEmptyBody")
     private void sayWinners(Scene scene, MediaView view){
         for(int i = 0; i < WINNERS; i++){
-            scene.setOnKeyPressed(null);
-            openNewVideo(view, SUSPANCE);
-            while(!view.getMediaPlayer().getStatus().equals(MediaPlayer.Status.STOPPED)){}
-            openNewVideo(view, String.format("%d.jpg", winner()));
-            MyBoolean keyListener = new MyBoolean();
-            scene.setOnKeyPressed(event -> keyListener.set(true));
-            while (!keyListener.get()){}
+            listener[0] = false;
+            openNewVideo(scene, view, SUSPANCE, null, () -> listener[0] = true);
+            while(!listener[0]){}
+            listener[1] = false;
+            openNewVideo(scene, view, String.format("%d.jpg", winner()), event -> listener[1] = true, null);
+            while (!listener[1]){}
         }
         endOfLife(scene, view);
     }
@@ -78,10 +66,12 @@ public class EmbeddedMediaPlayer extends Application {
         return w;
     }
 
-    private void openNewVideo(MediaView view, String path){
+    private void openNewVideo(Scene scene, MediaView view, String path, EventHandler<KeyEvent> event, Runnable onEnd){
         MediaPlayer player = new MediaPlayer(new Media(getClass().getResource(path).toExternalForm()));
+        player.setOnEndOfMedia(onEnd);
         player.setAutoPlay(true);
         view.setMediaPlayer(player);
+        scene.setOnKeyPressed(event);
     }
 
     public static void main(String[] args) {launch(args);}
